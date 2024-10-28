47c71697e3770527196d52fb038982d8834ae02b
GabrielZilmar <gabrielzilmar@hotmail.com>
2024-10-28 12:00:54 -0300

    feat: delete tweet feature

--- src/main/java/com/zilmar/springsecurity/modules/tweet/TweetController.java
+++ src/main/java/com/zilmar/springsecurity/modules/tweet/TweetController.java
@@ -2,16 +2,14 @@ package com.zilmar.springsecurity.modules.tweet;
 
 import com.zilmar.springsecurity.database.repositories.TweetRepository;
 import com.zilmar.springsecurity.database.repositories.UserRepository;
+import com.zilmar.springsecurity.modules.roles.enums.RolesValues;
 import com.zilmar.springsecurity.modules.tweet.dto.CreateTweetDTO;
 import com.zilmar.springsecurity.modules.tweet.entities.Tweet;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
@@ -39,5 +37,32 @@ public class TweetController {
         this.tweetRepository.save(tweet);
 
         return ResponseEntity.ok(tweet);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTweet(
+            @PathVariable("id") UUID id,
+            JwtAuthenticationToken token
+    ) {
+        var user = this.userRepository.findById(UUID.fromString(token.getName()));
+        if (user.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User doesn't exists.");
+        }
+
+        var tweet = this.tweetRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Tweet was not found."));
+        if (!tweet.getUser().getId().equals(user.get().getId())) {
+            var isAdmin = user.get().getRoles()
+                    .stream()
+                    .anyMatch(role -> role.getName().name().equalsIgnoreCase(RolesValues.ADMIN.name()));
+            if (!isAdmin) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You only can delete your own tweets.");
+            }
+        }
+
+        this.tweetRepository.deleteById(tweet.getId());
+
+        return ResponseEntity.ok().build();
     }
 }
