90044c172a2f9da0c88e37d8d56694e3164c9bf5
GabrielZilmar <gabrielzilmar@hotmail.com>
2024-10-28 11:40:40 -0300

    fix: add CreationTimestamp to Tweet create_at

--- src/main/java/com/zilmar/springsecurity/modules/tweet/entities/Tweet.java
+++ src/main/java/com/zilmar/springsecurity/modules/tweet/entities/Tweet.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -27,7 +28,8 @@ public class Tweet {
     private User user;
 
     private String content;
-    
+
+    @CreationTimestamp
     @Column(name = "created_at")
     private Instant createdAt;
 }
